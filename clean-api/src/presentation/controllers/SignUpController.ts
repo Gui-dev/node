754377f908e9cf8e1ade6d0f173b5a1c4e003f8c
Gui-dev <adrianon10@gmail.com>
2020-08-01 02:43:54 -0300

    feat: ensure SignUpController returns 400 if no password is provider

--- clean-api/src/presentation/controllers/SignUpController.ts
+++ clean-api/src/presentation/controllers/SignUpController.ts
@@ -4,11 +4,11 @@ import { badRequest } from './../helpers/httpHelpers'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredField = ['name', 'email', 'password']
+    for (const field of requiredField) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
     return badRequest(new MissingParamError('server'))
