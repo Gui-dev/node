4f887155a19953d159566d20d694bc5aac920b60
Gui-dev <adrianon10@gmail.com>
2020-08-06 02:27:36 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- clean-api/src/presentation/controllers/SignUpController.ts
+++ clean-api/src/presentation/controllers/SignUpController.ts
@@ -1,17 +1,20 @@
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from './../protocols'
 import { MissingParamError, InvalidParamError } from './../errors'
 import { badRequest, serverError } from './../helpers/httpHelpers'
+import { AddAccount } from '../../domain/usercases/AddAccount'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
       const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredField) {
         if (!httpRequest.body[field]) {
@@ -25,6 +28,8 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.add({ name, email, password })
     } catch {
       return serverError()
     }
