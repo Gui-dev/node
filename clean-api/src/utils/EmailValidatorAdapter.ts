fe97db9d2182921fbbd666a08f6723c57b144e5b
Gui-dev <adrianon10@gmail.com>
2020-08-08 03:00:59 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

--- clean-api/src/utils/EmailValidatorAdapter.ts
+++ clean-api/src/utils/EmailValidatorAdapter.ts
@@ -1,7 +1,7 @@
 import { EmailValidator } from './../presentation/protocols/EmailValidator'
 
 export class EmailValidatorAdapter implements EmailValidator {
-  isValid(email: string): boolean {
+  isValid (email: string): boolean {
     return false
   }
 }
