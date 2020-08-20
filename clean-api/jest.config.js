9feadb7a2ba858b08539ce24c2b29983a5ab95bf
Gui-dev <adrianon10@gmail.com>
2020-08-20 02:21:50 -0300

    chore: add jest configs for mongodb

--- clean-api/jest.config.js
+++ clean-api/jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-
 }
